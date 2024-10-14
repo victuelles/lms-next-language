d0f369a807a2d754cdc759d64d2943439aa30105
Rom Victuelles <rom.victuelles@gmail.com>
2024-10-14 13:22:04 -0700

    attachment form added and api

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,19 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import {
+  CircleDollarSign,
+  File,
+  LayoutDashboard,
+  ListChecks,
+} from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { courseId } = params;
@@ -20,6 +26,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     where: {
       id: courseId,
     },
+    include:{
+      attachments:{
+        orderBy:{
+          createdAt:"desc"
+        }
+      }
+    }
   });
 
   const categories = await db.category.findMany({
@@ -81,8 +94,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
             <IconBadge icon={CircleDollarSign} />
             <h2>Sell your course</h2>
           </div>
-          <PriceForm initialData={course} courseId={courseId}/>
+          <PriceForm initialData={course} courseId={courseId} />
+          <div className="flex items-center gap-x-2">
+          <IconBadge icon={File} />
+          <h2 className="text-xl">Resources & Attachments</h2>
         </div>
+        <AttachmentForm initialData={course} courseId={courseId} />
+        </div>
+
       </div>
     </div>
   );
