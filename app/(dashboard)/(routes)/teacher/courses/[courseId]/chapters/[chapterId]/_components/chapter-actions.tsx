07d0d4b165da374a8f2d73e6e3a2c5b013b2ced5
Rom Victuelles <rom.victuelles@gmail.com>
2024-10-16 15:17:19 -0700

    added chapter publish/unpublished apis

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -8,7 +8,6 @@ import { ConfirmModal } from "@/components/modals/confirm-modal";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
-
 interface ChapterActionsProps {
   disabled: boolean;
   courseId: string;
@@ -22,37 +21,58 @@ export const ChapterActions = ({
   chapterId,
   isPublished,
 }: ChapterActionsProps) => {
-    const router=useRouter()
-    const [isLoading,setIsLoading] =useState(false)
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const onDelete =async()=>{
-        try {
-            setIsLoading(true)
-            await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`)
-            toast.success("Chapter deleted.")
-            router.refresh()
-            router.push(`/teacher/courses/${courseId}`)
-        } catch (error) {
-            toast.error("Something went wrong")
-            
-        }finally{
-            setIsLoading(false)
-        }
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      if (isPublished) {
+        await axios.patch(
+          `/api/courses/${courseId}/chapters/${chapterId}/unpublish`
+        );
+        toast.success("Chapter unpublished");
+      } else {
+        await axios.patch(
+          `/api/courses/${courseId}/chapters/${chapterId}/publish`
+        );
+        toast.success("Chapter published");
+      }
+      router.refresh()
+    } catch (error) {
+      toast.error("Something went wrong")
+    }finally{
+      setIsLoading(false)
     }
+  };
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+      toast.success("Chapter deleted.");
+      router.refresh();
+      router.push(`/teacher/courses/${courseId}`);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="flex items-center gap-x-2">
       <Button
-        onClick={() => {}}
+        onClick={onClick}
         disabled={disabled || isLoading}
         variant={"outline"}
         size="sm"
       >
         {isPublished ? "Unpublish" : "Publish"}
       </Button>
-      <ConfirmModal onConfirm={onDelete} >
-      <Button size="sm" disabled={isLoading}>
-        <Trash className="h-4 w-4" />
-      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
       </ConfirmModal>
     </div>
   );
