ffdf58c60b7cbefdfa2fa39248fa9e055bbd77a5
Rom Victuelles <rom.victuelles@gmail.com>
2024-10-18 20:44:26 -0700

    nextConfig ignore during builds

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,12 @@ const nextConfig = {
                 pathname: '**',
               },
           ],
-    }
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
