7c9c40edeedb938528eb73ad1c80b6aab56456ae
Rom Victuelles <rom.victuelles@gmail.com>
2024-10-21 14:29:40 -0700

    updated button variant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -14,9 +14,6 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        // secondary:
-        //   "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         success: "bg-emerald-600 text-white hover:bg-emerald-600/80",
         primaryOutline: "bg-white text-sky-500 hover:bg-slate-100",
