ef76a30a847d6b99e9057f11b6d375521980b3be
Rom Victuelles <rom.victuelles@gmail.com>
2024-10-25 12:02:58 -0700

    added ga to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Nunito } from "next/font/google";
 import {
   ClerkProvider,
 } from '@clerk/nextjs'
+import Script from "next/script";
 import "./globals.css";
 import ToastProvider from "@/components/providers/toaster-provider";
 import { ConfettiProvider } from "@/components/providers/confetti-provider";
@@ -24,7 +25,7 @@ export const metadata: Metadata = {
   title: "TalkFilipino App",
   description: "(c)Rom 2024",
 };
-
+const ga=process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -49,6 +50,17 @@ export default function RootLayout({
     }}
   >
     <html lang="en">
+      <head>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${ga}" `} strategy="afterInteractive"/>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${ga}');
+          `}
+        </Script>
+      </head>
 
       <body  className={`min-h-screen flex flex-col antialiased ${font.className}`}>
 
